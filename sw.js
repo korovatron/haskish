85b5f90c0ff9da6a82b33f25babc64c351292f0d
Neil <neil@logan-5.co.uk>
2025-12-29 20:31:57 +0000

    Change cache name to force refresh - haskish-v88 -> haskish-cache-v1

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'haskish-v88';
+const CACHE_NAME = 'haskish-cache-v1';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
@@ -143,6 +143,7 @@ self.addEventListener('message', (event) => {
         self.skipWaiting();
     }
 });
+
 
 
 
