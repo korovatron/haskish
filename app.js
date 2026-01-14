ae96cc48d6d961e340372463658ab113feb16b66
Neil <neil@logan-5.co.uk>
2026-01-14 21:37:55 +0000

    made minor edit to countoccurences in lesson 10 and its solution in the solution file

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Haskish App - UI Controller
 
 // Version number
-const HASKISH_VERSION = '1.0.13';
+const HASKISH_VERSION = '1.0.14';
 
 const interpreter = new HaskishInterpreter();
 
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'haskish-v1.0.13';
+const CACHE_NAME = 'haskish-v1.0.14';
 const urlsToCache = [
   './',
   './index.html',
