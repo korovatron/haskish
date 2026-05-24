ae2644525f4476587dd43a3ba966d61028842418
Neil <neil@logan-5.co.uk>
2026-05-24 14:22:59 +0100

    Modified Mandelbrot example to reduce maxIter to ensure no timeout

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Haskish App - UI Controller
 
 // Version number
-const HASKISH_VERSION = '1.2.4';
+const HASKISH_VERSION = '1.2.5';
 
 const interpreter = new HaskishInterpreter();
 
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'haskish-v1.2.4';
+const CACHE_NAME = 'haskish-v1.2.5';
 const urlsToCache = [
   './',
   './index.html',
